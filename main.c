1eefcb84a5fd20b0bec2844aae9e603ccb5b04c4
Marcin <m.kosinskki@gmail.com>
2025-05-01 17:22:12 +0200

    Car struct, CarQueue struct, updated input arg check for cars count > 1

--- main.c
+++ main.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h> //lib with exit statuses
+#include <semaphore.h>
+#include <pthread.h>
+
+typedef struct Car {
+    int carNumber;
+    int location;
+    sem_t readyToCross;
+} Car;
+
+typedef struct CarQueue {
+    Car *cars[100];
+    int carFront;
+    int carRear;
+    int count;
+    pthread_mutex_t mutex;
+} CarQueue;
 
 int main (int argc, char *argv[])
 {
     if(argc != 2)
     {
         printf("Usage: %s N\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int N = atoi(argv[1]);
+    if(N <= 0)
+    {
+        printf("Cars count must be at least 1");
         exit(EXIT_FAILURE);
     }
 }
